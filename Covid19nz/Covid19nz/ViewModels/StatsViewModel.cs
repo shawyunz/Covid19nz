00352d60b139f9136f483a6afbe4be0f5824fcb3
Shaw Yu <shawyunz@gmail.com>
2020-04-08 19:40:25 +1200

    :bento: Update image source and allow to zoom in and out.

--- Covid19nz/Covid19nz/ViewModels/StatsViewModel.cs
+++ Covid19nz/Covid19nz/ViewModels/StatsViewModel.cs
@@ -18,6 +18,7 @@ namespace Covid19nz.ViewModels
         public ICommand OpenAlertPDFCommand { get; }
 
         public string ImageUrl => "https://tinyurl.com/covid19nz-stats";
+        public string WebsiteUrl => "http://shawyunz.c1.biz/";
         //public string WebSourceUrl => "https://covid19map.nz/stats";
         public string WebSourceUrl => "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-current-cases#summary";
     }
