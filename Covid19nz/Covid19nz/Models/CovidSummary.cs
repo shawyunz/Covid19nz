eaefdee903ac972143247efe0afb0f400ef96abf
Shaw Yu <shawyunz@gmail.com>
2020-04-11 23:57:11 +1200

    :bug: Wrong format when number less than zero.

--- Covid19nz/Covid19nz/Models/CovidSummary.cs
+++ Covid19nz/Covid19nz/Models/CovidSummary.cs
@@ -7,30 +7,43 @@ namespace Covid19nz.Models
     {
         public CovidSummary(Dictionary<string, int> SummaryData)
         {
-            ConfirmedCasesTotal     = SummaryData["ConfirmedCasesTotal"];
-            ConfirmedCasesNew24h    = SummaryData["ConfirmedCasesNew24h"];
-            ProbableCasesTotal      = SummaryData["ProbableCasesTotal"];
-            ProbableCasesNew24h     = SummaryData["ProbableCasesNew24h"];
-            RecoveredCasesTotal     = SummaryData["RecoveredCasesTotal"];
-            RecoveredCasesNew24h    = SummaryData["RecoveredCasesNew24h"];
+            ConfirmedCasesTotal  = SummaryData["ConfirmedCasesTotal"];
+            var cfm24            = SummaryData["ConfirmedCasesNew24h"];
+            ConfirmedCasesNew24h = cfm24 > 0 ? $"(+{cfm24})" : $"({cfm24})";    //need refactor
+
+            ProbableCasesTotal   = SummaryData["ProbableCasesTotal"];
+            var prb24            = SummaryData["ProbableCasesNew24h"];
+            ProbableCasesNew24h  = prb24 > 0 ? $"(+{prb24})" : $"({prb24})";
+
+            RecoveredCasesTotal  = SummaryData["RecoveredCasesTotal"];
+            var rcv24            = SummaryData["RecoveredCasesNew24h"];
+            RecoveredCasesNew24h = rcv24 > 0 ? $"(+{rcv24})" : $"({rcv24})";
+
             HospitalisedCasesTotal  = SummaryData["HospitalisedCasesTotal"];
-            HospitalisedCasesNew24h = SummaryData["HospitalisedCasesNew24h"];
-            DeathCasesTotal         = SummaryData["DeathCasesTotal"];
-            DeathCasesNew24h        = SummaryData["DeathCasesNew24h"];
+            var hsp24               = SummaryData["HospitalisedCasesNew24h"];
+            HospitalisedCasesNew24h = hsp24 > 0 ? $"(+{hsp24})" : $"({hsp24})";
+
+            DeathCasesTotal      = SummaryData["DeathCasesTotal"];
+            var dth24            = SummaryData["DeathCasesNew24h"];
+            DeathCasesNew24h     = dth24 > 0 ? $"(+{dth24})" : $"({dth24})";
+
+            CasesAmount          = ConfirmedCasesTotal + ProbableCasesTotal;
+            var amt24            = cfm24 + prb24;
+            CasesAmountNew24h    = amt24 > 0 ? $"(+{amt24})" : $"({amt24})";
         }
 
         public int ConfirmedCasesTotal { get; set; }
-        public int ConfirmedCasesNew24h { get; set; }
+        public string ConfirmedCasesNew24h { get; set; }
         public int ProbableCasesTotal { get; set; }
-        public int ProbableCasesNew24h { get; set; }
+        public string ProbableCasesNew24h { get; set; }
         public int RecoveredCasesTotal { get; set; }
-        public int RecoveredCasesNew24h { get; set; }
+        public string RecoveredCasesNew24h { get; set; }
         public int HospitalisedCasesTotal { get; set; }
-        public int HospitalisedCasesNew24h { get; set; }
+        public string HospitalisedCasesNew24h { get; set; }
         public int DeathCasesTotal { get; set; }
-        public int DeathCasesNew24h { get; set; }
-        public int CasesAmount => ConfirmedCasesTotal + ProbableCasesTotal;
-        public int CasesAmountNew24h => ConfirmedCasesNew24h + ProbableCasesNew24h;
+        public string DeathCasesNew24h { get; set; }
+        public int CasesAmount { get; set; }
+        public string CasesAmountNew24h { get; set; }
     }
 
     public partial class CovidSummary
