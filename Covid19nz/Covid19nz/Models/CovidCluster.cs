f714003be6c3bd1d26d686f0c914f7a4d442e200
Shaw Yu <shawyunz@gmail.com>
2020-04-28 00:58:21 +1200

    :alien: Add API for cluster info.

---
+++ Covid19nz/Covid19nz/Models/CovidCluster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Covid19nz.Models
+{
+
+    public partial class CovidCluster
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Location")]
+        public string Location { get; set; }
+
+        [JsonProperty("Cases")]
+        public long Cases { get; set; }
+
+        [JsonProperty("CasesNew24h")]
+        public long CasesNew24H { get; set; }
+    }
+
+    public partial class CovidCluster
+    {
+        public static List<CovidCluster> FromJson(string json) => JsonConvert.DeserializeObject<List<CovidCluster>>(json, Converter.Settings);
+
+        //////sample data
+        ////{
+        ////  "Name": "Aged residential care facility (1)",
+        ////  "Location": "Christchurch",
+        ////  "Cases": 54,
+        ////  "CasesNew24h": 2
+        ////},
+    }
+}
